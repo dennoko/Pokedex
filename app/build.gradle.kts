b61bc92a266cf7d20552f6ed3297b0c57ba6112a
dennoko <sd514dennoko@gmail.com>
2024-03-31 15:35:31 +0900

    hilt を導入しようとして詰まった

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     // serialization plugin
     kotlin("plugin.serialization") version "1.9.23"
     // ksp
-    id("com.google.devtools.ksp") version "1.5.30-1.0.0"
+    id("com.google.devtools.ksp") version "1.6.0-1.0.0"
 }
 
 android {
